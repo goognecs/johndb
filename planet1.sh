da7d3d959e94baf657308bbc342f89c4a08d3523
Necs <goognecs@gmail.com>
2025-03-30 18:58:50 +0200

    resolved conflict on planet1 file

--- planet1.sh
+++ planet1.sh
@@ -4,10 +4,7 @@ And
 
 You can blame no one for it.
 
-<<<<<<< HEAD
 What if the Moon turn's Red
 What if the Moon turn's Red
 I think that's not a natural phenomenum
 It's weird
-=======
->>>>>>> fe00327375dafed27b742d89b0e0dbdb4c3e0da3
